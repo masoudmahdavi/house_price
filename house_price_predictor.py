0a7ef43b86ad1ca85b677c9519e8de1e5884139c
masoud <masoudmahdavi1995@gmail.com>
2024-10-04 18:25:56 +0330

    add plot histogram on terminal option(it was not good idea:/)

--- house_price_predictor.py
+++ house_price_predictor.py
@@ -2,6 +2,8 @@ from typing import Any
 from utils.data import house_dataframe
 from model.model import Model
 import matplotlib.pyplot as plt
+import termplotlib as tpl
+import numpy as np
 
 class HousePrice:
     def __init__(self, model:Model) -> None:
@@ -20,6 +22,17 @@ class HousePrice:
         if hist:
             self.house_data_dataframe.hist(bins=100, figsize=(12, 8))
             plt.show()
+            # for column in self.house_data_dataframe.select_dtypes(include=np.number).columns:
+            #     data = self.house_data_dataframe[column].to_numpy()
+                
+            #     hist, bins = np.histogram(data, bins=100)
+            #     fig = tpl.figure()
+            #     fig.hist(hist, bins, orientation='vertical')
+
+            #     print(f"Histogram for {column}:")
+            #     fig.show()
+
+
 if __name__ == "__main__":
     model = Model()
     house_price = HousePrice(model)
